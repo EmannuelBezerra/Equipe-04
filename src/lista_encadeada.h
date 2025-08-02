74f20c69048392b97bc256a808710d81f84dcbb4
José Carlos <167440479+carloszx7@users.noreply.github.com>
2025-08-02 20:59:03 -0300

    Update lista_encadeada.h

--- src/lista_encadeada.h
+++ src/lista_encadeada.h
@@ -1,11 +1,12 @@
 #ifndef LISTA_ENCADEADA_H
 #define LISTA_ENCADEADA_H
 
+// Estrutura de nó para lista encadeada
 typedef struct Node {
     int data;
     struct Node *next;
 } Node;
-
+// Funções básicas da lista
 Node* criar_lista(int *valores, int n);
 void imprimir_lista(Node *head);
 void liberar_lista(Node *head);
