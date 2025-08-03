35dddd6ba1835aa18ada649cce775ea5b034d459
José Carlos <167440479+carloszx7@users.noreply.github.com>
2025-08-02 21:05:45 -0300

    Update shell_sort.h

--- src/shell_sort.h
+++ src/shell_sort.h
@@ -3,9 +3,11 @@
 
 #include "lista_encadeada.h"
 
+// Variáveis globais para contagem de comparações e trocas
 extern int trocas;
 extern int comparacoes;
 
+// Função principal de ordenação Shell Sort
 void shellSortLista(Node *head);
 
 #endif
